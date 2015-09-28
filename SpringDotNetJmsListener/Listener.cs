f70f439a4156fd391b6b31cdcb9efc22482defe1
mshamma <mshamma@berkeley.edu>
2015-09-27 23:35:41 -0700

    Listener class for SimpleMessageContainer

---
+++ SpringDotNetJmsListener/Listener.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Apache.NMS;
+using Spring.Messaging.Nms.Core;
+
+namespace SpringDotNetJmsListener
+{
+    class Listener : IMessageListener
+    {
+        public Listener()
+        {
+            Console.WriteLine("Listener created.\r\n");
+        }
+
+        #region IMessageListener Members
+
+        public void OnMessage(IMessage message)
+        {
+            ITextMessage textMessage = message as ITextMessage;
+            Console.WriteLine(textMessage.Text);
+        }
+
+        #endregion
+    }
+}
