9d7c90fd2177db8693c98652d379c09f4555e447
mshamma <mshamma@berkeley.edu>
2015-09-27 23:35:22 -0700

    Added connection factory, message container and listener

--- SpringDotNetJmsListener/Program.cs
+++ SpringDotNetJmsListener/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using Apache.NMS.ActiveMQ;
+using Spring.Messaging.Nms.Listener;
 
 namespace SpringDotNetJmsListener
 {
     class Program
     {
+        private static string URI = ConfigurationManager.AppSettings["connectUri"];
+        private static string DESTINATION = ConfigurationManager.AppSettings["destination"];
+        private static string USER = ConfigurationManager.AppSettings["userName"];
+        private static string PASS = ConfigurationManager.AppSettings["userPassword"];
+
         static void Main(string[] args)
         {
+            try
+            {
+                ConnectionFactory connectionFactory = new ConnectionFactory(URI);
+                connectionFactory.UserName = USER;
+                connectionFactory.Password = PASS;
+
+                using (SimpleMessageListenerContainer listenerContainer = new SimpleMessageListenerContainer())
+                {
+                    listenerContainer.ConnectionFactory = connectionFactory;
+                    listenerContainer.DestinationName = DESTINATION;
+                    listenerContainer.MessageListener = new Listener();
+                    listenerContainer.AfterPropertiesSet();
+                    Console.WriteLine("Listener started.");
+                    Console.WriteLine("Press <ENTER> to exit.");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Press <ENTER> to exit.");
+                Console.Read();
+            }
+            // comment when testing
+            Console.ReadLine();
         }
     }
 }
